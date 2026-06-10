02461cb263812ed8bec9b2cc99700e64471e488b
Mohamed <moshindo727@gmail.com>
2026-06-10 06:12:36 +0300

    fix: remove dist from tracking

--- dist/src/database/prisma.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-export declare class PrismaService implements OnModuleInit, OnModuleDestroy {
-    onModuleInit(): Promise<void>;
-    onModuleDestroy(): Promise<void>;
-}
